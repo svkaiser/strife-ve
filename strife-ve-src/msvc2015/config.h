1990bf4327af10761efc774ded33b3230406f96f
James Haley <haleyjd@hotmail.com>
2020-10-24 23:16:40 -0500

    Missing config.h for msvc

---
+++ strife-ve-src/msvc2015/config.h
@@ -0,0 +1,31 @@
+/* config.h for MSVC builds.  This is just the config.h created by
+ * configure. */
+
+/* Name of package */
+#define PACKAGE "strife-ve"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "nightdivestudios@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Strife: Veteran Edition"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Strife-VE 1.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "strife-ve"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.0"
+
+/* Change this when you create your awesome forked version */
+#define PROGRAM_PREFIX "chocolate-"
+
+/* Version number of package */
+#define VERSION "1.0.0"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
